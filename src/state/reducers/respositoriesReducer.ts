cc3bbbaff3e8063106899d837f085cd8a77f9471
samroodAli <devsamrood@gmail.com>
2021-10-13 19:33:54 +0530

    Refactor: Refactor to give action interfaces for different actions

--- src/state/reducers/respositoriesReducer.ts
+++ src/state/reducers/respositoriesReducer.ts
@@ -4,14 +4,26 @@ interface RepositoriesState {
   data: string[];
 }
 
-interface Action {
-  type: string;
-  payload?: any;
+interface SearchRepositoriesAction {
+  type: "search_repositories";
+}
+
+interface SearchRepositoriesSuccessAction {
+  type: "search_repositories_success";
+  payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+  type: "search_repositories_error";
+  payload: string;
 }
 
 const reducer = (
   state: RepositoriesState,
-  action: Action
+  action:
+    | SearchRepositoriesAction
+    | SearchRepositoriesSuccessAction
+    | SearchRepositoriesErrorAction
 ): RepositoriesState => {
   switch (action.type) {
     case "search_repositories":
