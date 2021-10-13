837fec194c7b9525d5e8a3b2ce0e85370e06b430
samroodAli <devsamrood@gmail.com>
2021-10-13 20:45:42 +0530

    Refactor: Refactor use enums to organise likewise data on action types

--- src/state/reducers/respositoriesReducer.ts
+++ src/state/reducers/respositoriesReducer.ts
@@ -4,32 +4,39 @@ interface RepositoriesState {
   data: string[];
 }
 
+enum ActionType {
+  SEARCH_REPOSITORIES = "SEARCH_REPOSITORIES",
+  SEARCH_REPOSITORIES_SUCCESS = "SEARCH_REPOSITORIES_SUCCESS",
+  SEARCH_REPOSITORIES_ERROR = "SEARCH_REPOSITORIES_ERROR",
+}
 interface SearchRepositoriesAction {
-  type: "search_repositories";
+  type: ActionType.SEARCH_REPOSITORIES;
 }
 
 interface SearchRepositoriesSuccessAction {
-  type: "search_repositories_success";
+  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
   payload: string[];
 }
 interface SearchRepositoriesErrorAction {
-  type: "search_repositories_error";
+  type: ActionType.SEARCH_REPOSITORIES_ERROR;
   payload: string;
 }
 
+type Action =
+  | SearchRepositoriesAction
+  | SearchRepositoriesSuccessAction
+  | SearchRepositoriesErrorAction;
+
 const reducer = (
   state: RepositoriesState,
-  action:
-    | SearchRepositoriesAction
-    | SearchRepositoriesSuccessAction
-    | SearchRepositoriesErrorAction
+  action: Action
 ): RepositoriesState => {
   switch (action.type) {
-    case "search_repositories":
+    case "SEARCH_REPOSITORIES":
       return { loading: true, error: null, data: [] };
-    case "search_repositories_success":
+    case "SEARCH_REPOSITORIES_SUCCESS":
       return { loading: false, error: null, data: action.payload };
-    case "search_repositories_error":
+    case "SEARCH_REPOSITORIES_ERROR":
       return { loading: false, error: action.payload, data: [] };
     default:
       return state;
