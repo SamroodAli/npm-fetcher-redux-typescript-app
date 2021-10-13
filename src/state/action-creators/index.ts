42654841c2fe2c8e5cbd8e94041a59f8657ccc04
samroodAli <devsamrood@gmail.com>
2021-10-13 21:31:08 +0530

    Action-creator:Create action creator to return async function for redux thunk

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,3 +1,34 @@
 import axios from "axios";
 import { ActionTypes } from "../action-types";
 import { Action } from "../actions";
+
+export const searchRepositoris = (searchTerm: string) => {
+  return async (dispatch: any) => {
+    dispatch({ type: ActionTypes.SEARCH_REPOSITORIES });
+
+    try {
+      const { data } = await axios.get(
+        "https://registry.npmjs.org/-/v1/search",
+        {
+          params: {
+            text: searchTerm,
+          },
+        }
+      );
+
+      const names = data.object.map((result: any) => {
+        return result.package.name;
+      });
+
+      dispatch({
+        type: ActionTypes.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names,
+      });
+    } catch (error: any) {
+      dispatch({
+        type: ActionTypes.SEARCH_REPOSITORIES_ERROR,
+        payload: error.message,
+      });
+    }
+  };
+};
