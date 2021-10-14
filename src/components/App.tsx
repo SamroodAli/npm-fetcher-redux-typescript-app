40f551f1ee6dd692d380773e97ec52a91154140f
samroodAli <devsamrood@gmail.com>
2021-10-14 11:33:20 +0530

    Component:Import and use repositoreslist component in App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,13 @@
 import { store } from "../redux";
 import { Provider } from "react-redux";
+import RepositoriesList from "./RepositoresList";
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
         <h1>Search for a package</h1>
+        <RepositoriesList />
       </div>
     </Provider>
   );
