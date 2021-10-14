d32cfefe42419aaf7088ec22e0bd968ce4f0eb0f
samroodAli <devsamrood@gmail.com>
2021-10-14 11:34:07 +0530

    Fix: Give initialState for redux

--- src/redux/reducers/respositoriesReducer.ts
+++ src/redux/reducers/respositoriesReducer.ts
@@ -7,8 +7,14 @@ interface RepositoriesState {
   data: string[];
 }
 
+const initialState = {
+  loading: false,
+  error: null,
+  data: [],
+};
+
 const reducer = (
-  state: RepositoriesState,
+  state: RepositoriesState = initialState,
   action: Action
 ): RepositoriesState => {
   switch (action.type) {
